700afe473b68b5e3743026d80f581f011e19d7eb
wremsen <wcremsen@gmail.com>
2023-10-31 17:50:16 -0400

    third commit

--- practice-for-ch-ruby-recursion-long-practice-main/practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
@@ -11,8 +11,31 @@ end
 # p exponent(2, 2)
 # p exponent(2, 4)
 
-def deep_dup
 
+# 
+def deep_dup(arr)
+ return [arr] if !arr.is_a?(Array)
 
-    
+ dupe = []
+
+    arr.each do |ele|
+        if !ele.is_a?(Array)
+        dupe << ele
+        else
+        dupe += deep_dup(ele)
+        end
+    end
+
+    return dupe
 end
+
+robot_parts = [
+  ["nuts", "bolts", "washers"],
+  ["capacitors", "resistors", "inductors"]
+]
+
+robot_parts_copy = deep_dup(robot_parts)
+
+robot_parts_copy[1] << "LEDs"
+
+p robot_parts[1]
