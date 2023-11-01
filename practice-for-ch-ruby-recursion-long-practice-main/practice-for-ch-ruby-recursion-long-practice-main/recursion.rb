c46093f91de8c9dd4d72fa3dc6683b1f231175fe
Almoe099 <almoe099@gmail.com>
2023-11-01 14:21:53 -0400

    bsearch

--- practice-for-ch-ruby-recursion-long-practice-main/practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
@@ -29,13 +29,55 @@ def deep_dup(arr)
     return dupe
 end
 
-robot_parts = [
-  ["nuts", "bolts", "washers"],
-  ["capacitors", "resistors", "inductors"]
-]
+# robot_parts = [
+#   ["nuts", "bolts", "washers"],
+#   ["capacitors", "resistors", "inductors"]
+# ]
 
-robot_parts_copy = deep_dup(robot_parts)
+# robot_parts_copy = deep_dup(robot_parts)
 
-robot_parts_copy[1] << "LEDs"
+# robot_parts_copy[1] << "LEDs"
 
-p robot_parts[1]
+# p robot_parts[1]
+
+def fibonacci(n)
+    return [] if n == 0
+    return [0] if n == 1
+    return [0, 1] if n == 2
+    
+    prev_arr = fibonacci(n-1)
+    prev_arr << prev_arr[-1] + prev_arr[-2]
+
+end
+
+# p fibonacci(6)
+
+def bsearch(arr, n)
+    return nil if arr.count(n) == 0
+    return 0 if arr.length == 1
+
+    # sorted = arr.sort
+    # mid_idx = sorted.length / 2
+
+    # if sorted[mid_idx] == n
+    #     return mid_idx
+    # elsif sorted[mid_idx] > n
+    #     bsearch(arr[0...mid_idx], n)
+    # elsif sorted[mid_idx] < n
+    #     bsearch(arr[mid_idx+1..-1], n)
+    # end
+
+
+
+    # arr[1..-1]
+
+
+end
+
+p bsearch([1, 2, 3], 1) # => 0
+p bsearch([2, 3, 4, 5], 3) # => 1
+p bsearch([2, 4, 6, 8, 10], 6) # => 2
+p bsearch([1, 3, 4, 5, 9], 5) # => 3
+p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
